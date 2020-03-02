228adad763ab11ebee827b1c2272955511f225ac
nirali <nirali@idrsolutions.com>
2020-03-02 11:55:32 +0000

    "inital js client commit "

---
+++ README-BUILDVU.md
@@ -0,0 +1,73 @@
+# BuildVu JavaScript Client #
+
+Convert PDF to HTML5 or SVG with JavaScript, using the BuildVu JavaScript Client to interact with IDRsolutions' [BuildVu Microservice Example](https://github.com/idrsolutions/buildvu-microservice-example).
+
+The BuildVu Microservice Example is an open source project that allows you to convert PDF to HTML5 or SVG by running [BuildVu](https://www.idrsolutions.com/buildvu/) as an online service. 
+
+IDRsolutions offer a free trial service for running BuildVu with JavaScript, more infomation on this can be found [here](https://www.idrsolutions.com/buildvu/convert-pdf-in-javascript/).
+
+
+-----
+
+# Installation
+
+```
+<script src="path/to/idrcloudclient.js" type="text/javascript"></script>
+```
+
+-----
+
+# Usage #
+
+```javascript
+var endpoint = "http://localhost:8080/ + IDRCLoudClient.BUILDVU";
+
+//Convert the attached file
+var file = document.getElementById('file-input').files[0];
+//Convert file from url (file takes precedence over this option).
+var url = document.getElementById('url-input').value;
+
+//Upload is used when a file uploaded to the server
+//Download is used when the server downloads from the url
+var input = file ? IDRCLoudClient.UPLOAD : IDRCLoudClient.DOWNLOAD;
+
+IDRCLoudClient.convert({
+    endpoint: endpoint,
+    //Parameters are the API parameters that will be sent to the server
+    //See https://github.com/idrsolutions/buildvu-microservice-example/blob/master/API.md
+    parameters: {
+        input: input,
+        file: file,
+        url: url
+    },
+    progress: function() { },
+    success: function(e) {
+        console.log("Converted " + e.downloadUrl);
+    },
+    failure: function() { }
+});
+```
+
+-----
+
+# Who do I talk to? #
+
+Found a bug, or have a suggestion / improvement? Let us know through the Issues page.
+
+Got questions? You can contact us [here](https://idrsolutions.zendesk.com/hc/en-us/requests/new).
+
+-----
+
+Copyright 2018 IDRsolutions
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+[http://www.apache.org/licenses/LICENSE-2.0](http://www.apache.org/licenses/LICENSE-2.0)
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ idrcloudclient.js
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2018 IDRsolutions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+(function () {
+    window.IDRCloudClient = (function () {
+
+        var progress, success, failure;
+
+
+
+        var doPoll = function (uuid, endpoint) {
+            var req, retries = 0;
+
+            var poll = setInterval(function () {
+                if (!req) {
+                    req = new XMLHttpRequest();
+                    req.onreadystatechange = function (e) {
+                        if (req.readyState === 4) {
+                            if (req.status === 200) {
+                                var data = JSON.parse(req.responseText);
+                                if (data.state === "processed") {
+                                    clearInterval(poll);
+                                    if (success) {
+                                        success(data);
+                                    }
+                                } else {
+                                    if (progress) {
+                                        progress(data);
+                                    }
+                                }
+                            } else {
+                                retries++;
+                                if (retries > 3) {
+                                    clearInterval(poll);
+                                    if (failure) {
+                                        failure("Connection error while polling status.");
+                                    }
+                                }
+                            }
+                            req = null;
+                        }
+                    };
+                    req.open("GET", endpoint + "?uuid=" + uuid , true);
+                    req.send();
+                }
+            }, 500);
+        };
+
+        return {
+            UPLOAD: 'upload',
+            DOWNLOAD: 'download',
+            JPEDAL: 'jpedal',
+            BUILDVU: 'buildvu' ,
+            convert: function (params) {
+
+                if (!params.endpoint) {
+                    throw Error('Missing endpoint');
+                }
+                if (params.success && typeof params.success === "function") {
+                    success = params.success;
+                }
+                if (params.failure && typeof params.failure === "function") {
+                    failure = params.failure;
+                }
+                if (params.progress && typeof params.progress === "function") {
+                    progress = params.progress;
+                }
+
+                var xhr = new XMLHttpRequest();
+                if (xhr.upload) {
+                    xhr.upload.addEventListener("progress", function (e) {
+                        if (progress) {
+                            progress({
+                                state: 'uploading',
+                                loaded: e.loaded,
+                                total: e.total
+                            });
+                        }
+                    }, false);
+
+                    xhr.onreadystatechange = function () {
+                        if (xhr.readyState === 4) {
+                            if (xhr.status === 200) {
+                                if (!params.parameters.callbackUrl || progress || success) {
+                                    doPoll(JSON.parse(xhr.responseText).uuid, params.endpoint);
+                                }
+                            } else {
+                                if (failure) {
+                                    try {
+                                        var parsedResponse = JSON.parse(xhr.responseText);
+                                        if (parsedResponse.hasOwnProperty("error")) {
+                                            failure(parsedResponse.error);
+                                        } else {
+                                            failure("Connection error. Status: " + xhr.status + ". Response: " + xhr.responseText);
+                                        }
+                                    } catch (e) {
+                                        failure("Connection error. Status: " + xhr.status + ". Response: " + xhr.responseText);
+                                    }
+                                }
+                            }
+                        }
+                    };
+
+                    xhr.open("POST", params.endpoint, true);
+                    var data = new FormData();
+
+                    if (params.parameters) {
+                        for (var prop in params.parameters) {
+                            if (params.parameters.hasOwnProperty(prop) && params.parameters[prop] !== undefined) {
+                                data.append(prop, params.parameters[prop]);
+                            }
+                        }
+                    }
+                    xhr.send(data);
+                }
+            }
+        };
+    })();
+
+})();
